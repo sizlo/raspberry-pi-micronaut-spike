f5c8594794d4d080ad1f40b580847164f660c259
Tim Summerton-Brier <t.brier93@gmail.com>
2023-11-27 16:57:08 +0000

    Add release/deploy scripts

---
+++ deployment-resources/deploy.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+log () {
+  DATE=$(date "+%Y-%m-%d_%H:%M:%S.%N")
+  echo "$DATE - $@"
+}
+
+APP_NAME=raspberry-pi-micronaut-spike
+GITHUB_USER=sizlo
+
+GITHUB_REPO=https://github.com/$GITHUB_USER/$APP_NAME
+GITHUB_REPO_API=https://api.github.com/repos/$GITHUB_USER/$APP_NAME
+FOLDER=$HOME/$APP_NAME
+
+# jq is installed here, but when run from cron this directory is not on the path
+PATH=$PATH:/usr/bin
+
+{
+  log "Deployment started"
+
+  # Install java if required
+  log "Checking for java 17 on the PATH"
+  if java --version | grep -q "\b17\b"; then
+    JAVA_COMMAND="java"
+    log "Found java 17 on the PATH"
+  else
+    log "Could not find java 17 on the PATH"
+    log "Checking for java 17 in app folder"
+    if $FOLDER/jdk-17-ga/bin/java --version | grep -q "\b17\b"; then
+      JAVA_COMMAND=$FOLDER/jdk-17-ga/bin/java
+      log "Found java 17 in app folder"
+    else
+      log "Could not find java 17 in app folder"
+      log "Installing raspberry pi 1 compatible jdk..."
+      wget -P $FOLDER $GITHUB_REPO/raw/main/deployment-resources/jdk-17-ga_gcc-10.1_glibc-2.28_binutils-2.31_Buster.tar.gz
+      tar zxf $FOLDER/jdk-17-ga_gcc-10.1_glibc-2.28_binutils-2.31_Buster.tar.gz -C $FOLDER
+      log "Raspberry pi 1 compatible jdk installed"
+      JAVA_COMMAND=$FOLDER/jdk-17-ga/bin/java
+    fi
+  fi
+
+  # Install jq if required
+  log "Checking for jq on the PATH"
+  if which jq &> /dev/null
+  then
+    log "Found jq on the PATH"
+  else
+    log "Installing jq"
+    sudo apt install jq -y
+    log "jq installed"
+  fi
+
+  log "Getting latest release info"
+  RELEASE_JSON="$(curl $GITHUB_REPO_API/releases/latest)"
+  JAR_FILENAME="$(echo $RELEASE_JSON | jq -r '.assets[0].name')"
+  URL="$(echo $RELEASE_JSON | jq -r '.assets[0].browser_download_url')"
+  RELEASE_JAR_SHA1SUM="$(echo $RELEASE_JSON | jq -r '.body' | grep '^jar sha1sum:' | cut -f 3 -d ' ')"
+  log "Got latest release info"
+
+  EXISTING_JAR_FILEPATH=$(ls $FOLDER/*.jar)
+  EXISTING_JAR_SHA1SUM=$(sha1sum $EXISTING_JAR_FILEPATH | cut -f 1 -d " ")
+
+  if [ $EXISTING_JAR_SHA1SUM = $RELEASE_JAR_SHA1SUM ]
+  then
+    log "Local jar sha1sum matches release jar sha1sum, skipping jar download"
+    JAR_FILEPATH=$EXISTING_JAR_FILEPATH
+  else
+    log "Deleting previous jars"
+    rm -rf $FOLDER/*.jar
+    log "Previous jars deleted"
+    log "Downloading jar from release"
+    wget -P $FOLDER $URL
+    log "jar downloaded"
+    JAR_FILEPATH=$FOLDER/$JAR_FILENAME
+  fi
+
+  log "Removing stale deployment logs"
+  ls $FOLDER/deploy_*.log | sort -r | tail +6 | xargs -I {} rm {}
+  log "Removed stale deployment logs"
+
+  log "Sourcing app environment from file"
+  . $FOLDER/raspberrypi.env
+  log "Sourced app environment"
+
+  log "Deployment complete, running jar"
+} > $FOLDER/deploy_$(date "+%Y-%m-%d_%H:%M:%S.%N").log 2>&1
+
+$JAVA_COMMAND -jar $JAR_FILEPATH
---
+++ release.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+set -e
+
+gitCommitAndPush () {
+  echo "Committing and pushing changes"
+  git add .
+  git commit -m "$1"
+  git push
+}
+
+getVersionFromLatestGitRelease () {
+  gh release view 2> /dev/null | grep "^tag:" | cut -f 2 -w | sed "s/v//"
+}
+
+getVersionFromBuildGradle () {
+  grep 'version\s*=\s*"\d*\.\d*\.\d*.*"' build.gradle.kts | cut -f 2 -d '"'
+}
+
+upgradeVersion () {
+  CURRENT_VERSION=$(getVersionFromBuildGradle)
+  NEW_VERSION=$1
+
+  if [[ $CURRENT_VERSION != $NEW_VERSION ]]
+  then
+    echo "Upgrading version in build files from $CURRENT_VERSION to $NEW_VERSION"
+    sed -i "" "s/version[ \t]*=[ \t]*\"$CURRENT_VERSION\"/version = \"$NEW_VERSION\"/g" build.gradle.kts
+    gitCommitAndPush "Upgrade version from $CURRENT_VERSION to $NEW_VERSION"
+  fi
+}
+
+RELEASE_TYPE=$1
+
+CURRENT_BRANCH=$(git branch --show-current)
+if [[ $CURRENT_BRANCH != master && $CURRENT_BRANCH != main ]]
+then
+  echo The release script must be run from the main branch
+  exit 1
+fi
+
+if [[ $(git status --porcelain) ]]
+then
+  echo Your local git repo has changes, releases should be done on clean repos
+  exit 1
+fi
+
+if [[ $RELEASE_TYPE != major && $RELEASE_TYPE != minor && $RELEASE_TYPE != patch ]]
+then
+  echo Please provide the release type: major minor or patch
+  exit 1
+fi
+
+STARTING_VERSION=$(getVersionFromLatestGitRelease)
+
+if [[ -z "${STARTING_VERSION}" ]]
+then
+  STARTING_VERSION=0.0.0
+  echo "No github release found, starting from version 0.0.1"
+else
+  echo "Found version $STARTING_VERSION from latest git release"
+fi
+
+STARTING_MAJOR=$(echo $STARTING_VERSION | cut -f 1 -d .)
+STARTING_MINOR=$(echo $STARTING_VERSION | cut -f 2 -d .)
+STARTING_PATCH=$(echo $STARTING_VERSION | cut -f 3 -d .)
+
+if [[ $RELEASE_TYPE == major ]]
+then
+  RELEASE_MAJOR=$(($STARTING_MAJOR + 1))
+  RELEASE_MINOR=0
+  RELEASE_PATCH=0
+elif [[ $RELEASE_TYPE == minor ]]
+then
+  RELEASE_MAJOR=$STARTING_MAJOR
+  RELEASE_MINOR=$(($STARTING_MINOR + 1))
+  RELEASE_PATCH=0
+else
+  RELEASE_MAJOR=$STARTING_MAJOR
+  RELEASE_MINOR=$STARTING_MINOR
+  RELEASE_PATCH=$((STARTING_PATCH + 1))
+fi
+
+RELEASE_VERSION="$RELEASE_MAJOR.$RELEASE_MINOR.$RELEASE_PATCH"
+
+echo "Releasing version $RELEASE_VERSION"
+
+if [[ $RELEASE_VERSION != $STARTING_VERSION ]]
+then
+  upgradeVersion $RELEASE_VERSION
+fi
+
+JAR_PATH="build/libs/raspberrypimicronautspike-$RELEASE_VERSION-all-optimized.jar"
+
+echo "Building jar"
+./gradlew clean build
+JAR_SHA1SUM=$(sha1sum $JAR_PATH | cut -f 1 -d " ")
+
+echo "Creating github release $RELEASE_VERSION"
+gh release create \
+  "v$RELEASE_VERSION" \
+  $JAR_PATH \
+  --title $RELEASE_VERSION \
+  --notes "jar sha1sum: $JAR_SHA1SUM" \
+  --latest
+
+SNAPSHOT_VERSION="$RELEASE_MAJOR.$RELEASE_MINOR.$((RELEASE_PATCH + 1))-SNAPSHOT"
+upgradeVersion $SNAPSHOT_VERSION
+
+
